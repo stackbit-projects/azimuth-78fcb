b81fdf0059b5a3f83fdb1ad5c0bce935efafba24
Stackbit Code Editor <projects@stackbit.com>
2021-06-11 23:13:04 +0000

    SectionContent.js: changed

--- src/components/SectionContent.js
+++ src/components/SectionContent.js
@@ -27,8 +27,7 @@ export default class SectionContent extends React.Component {
                         <div className="cell block-content">
                             {title && <h2 className="block-title underline">{title}</h2>}
                             {content && <div className="block-copy">{markdownify(content)}</div>}
-
-                           {actions && (
+                            {actions && (
                                 <div className="block-buttons">
                                     <CtaButtons actions={actions} />
                                 </div>
